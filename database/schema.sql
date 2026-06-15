0500d8d49636ab142942006863685baafde381b7
Anil <anilmende2002@gmail.com>
2026-06-15 16:11:39 +0530

    fixed issues for deployment

--- database/schema.sql
+++ database/schema.sql
@@ -11,5 +11,10 @@ CREATE TABLE github_profiles (
     following INT,
     profile_url VARCHAR(255),
     avatar_url TEXT,
-    account_created_at DATETIME
+    account_created_at DATETIME DEFAULT NULL,
+    account_age_years INT DEFAULT NULL,
+    language_stats TEXT,
+    top_language VARCHAR(100) DEFAULT NULL,
+    total_stars INT DEFAULT 0,
+    most_starred_repo VARCHAR(255) DEFAULT NULL
 );
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
 import pool from "./config/db.js";
 import githubRoutes from "./routes/githubRoutes.js";
 
